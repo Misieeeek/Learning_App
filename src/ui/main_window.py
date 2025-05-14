57f899769f685d8061f36a9193e33d9c10919a8f
Pawel Mis <kacper.mis02@gmail.com>
2025-05-14 23:51:45 +0200

    Making GUI structure, added names of learning techniques

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,16 +1,32 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
         self.setWindowTitle("Learning App")
         central_widget = QWidget()
-        layout = QVBoxLayout(central_widget)
         self.setCentralWidget(central_widget)
 
+        self.setStyleSheet("background-color: black;")
         screen = QApplication.primaryScreen()
-        screen_size = screen.size()
 
         rect = screen.availableGeometry()
         self.setGeometry(int(rect.width() / 4), int(rect.height() / 4), 1024, 600)
+
+        self.init_ui()
+
+    def init_ui(self):
+        button_names = [
+            "Home",
+            "Pomodoro",
+            "Quiz",
+            "Second Brain",
+            "Incremental Reading",
+            "Active Recall",
+            "Spaced Repetition",
+        ]
+        for i in range(len(button_names)):
+            btn = QPushButton(button_names[i], self)
+            btn.setGeometry(0, 50 * i + 50, 175, 50)
+            btn.setStyleSheet("background-color: #1a1a1a; color: white;")
