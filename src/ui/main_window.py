82337d47d5098a75ebbd6652e282acb0c6fc4d18
Pawel Mis <kacper.mis02@gmail.com>
2025-05-15 17:20:07 +0200

    WIP home screen

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QWidget
+
+from ui.widgets.week_histogram import WeekHistogram
 
 
 class MainWindow(QMainWindow):
@@ -8,7 +10,7 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        self.setStyleSheet("background-color: black;")
+        self.setStyleSheet("background-color: #0c1017;")
         screen = QApplication.primaryScreen()
 
         rect = screen.availableGeometry()
@@ -29,4 +31,34 @@ class MainWindow(QMainWindow):
         for i in range(len(button_names)):
             btn = QPushButton(button_names[i], self)
             btn.setGeometry(0, 50 * i + 50, 175, 50)
-            btn.setStyleSheet("background-color: #1a1a1a; color: white;")
+            btn.setStyleSheet("background-color: #192125; color: white;")
+        self.main_screen()
+
+    def main_screen(self):
+        lbl_week_activity = QLabel("This Week Activity", self)
+        lbl_week_activity.setGeometry(250, 100, 200, 25)
+        lbl_week_activity.setStyleSheet("color: white;")
+        self.week_activity()
+
+        lbl_overall_activity = QLabel("Overall Activity", self)
+        lbl_overall_activity.setGeometry(250, 300, 200, 25)
+        lbl_overall_activity.setStyleSheet("color: white;")
+        self.overall_activity()
+
+        lbl_todo = QLabel("To-Do list: ", self)
+        lbl_todo.setGeometry(750, 100, 200, 25)
+        lbl_todo.setStyleSheet("color: white;")
+        self.to_do_list()
+
+    def week_activity(self):
+        week_data = [24, 3, 8, 2, 6, 15, 4]
+        if hasattr(self, "week_widget"):
+            self.week_widget.setParent(None)
+        self.week_widget = WeekHistogram(parent=self, data=week_data)
+        self.week_widget.setGeometry(250, 130, 250, 125)
+
+    def overall_activity(self):
+        print("o")
+
+    def to_do_list(self):
+        print("t")
