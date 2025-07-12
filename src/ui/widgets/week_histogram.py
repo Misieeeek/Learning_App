e23104233008727ae0db6e9cc0baa508fd9ab82c
Pawel Mis <kacper.mis02@gmail.com>
2025-07-12 17:44:03 +0200

    Added hover border highlighting to week widget

--- src/ui/widgets/week_histogram.py
+++ src/ui/widgets/week_histogram.py
@@ -1,5 +1,5 @@
-from PyQt5.QtCore import QRect
-from PyQt5.QtGui import QBrush, QColor, QPainter, QPen
+from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import QBrush, QColor, QFont, QPainter, QPen
 from PyQt5.QtWidgets import QToolTip, QWidget
 
 
@@ -9,6 +9,9 @@ class Week_Histogram(QWidget):
         self.data = data or []
         self.setMouseTracking(True)
         self.hovered_index = None
+        self.labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        self.border_opacity = 150
+        self.border_direction = 1
         self.show()
 
     def paintEvent(self, event):
@@ -16,36 +19,41 @@ class Week_Histogram(QWidget):
             return
 
         qp = QPainter(self)
+        qp.setRenderHint(QPainter.Antialiasing)
+
         bar_width = self.width() // len(self.data)
         max_h = max(self.data)
 
-        lbls = [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-        ]
+        font = QFont()
+        font.setPointSize(10)
+        qp.setFont(font)
 
         for i, value in enumerate(self.data):
             x = i * bar_width
-            height = int((value / max_h) * (self.height() - 20))
-            y = self.height() - height
+            height = int((value / max_h) * (self.height() - 40))
+            y = self.height() - height - 20
 
             ratio = value / max_h
-            color = QColor(0, int(255 * (ratio)), 0)
+            fill_color = QColor(0, int(255 * ratio), 0)
 
-            brush = QBrush(color)
-            pen = QPen(QColor(255, 255, 255, 150))
+            brush = QBrush(fill_color)
+
+            if i == self.hovered_index:
+                pen_color = QColor(0, 128, 240, self.border_opacity)
+            else:
+                pen_color = QColor(255, 255, 255, 150)
+
+            pen = QPen(pen_color)
             pen.setWidth(2)
 
             qp.setBrush(brush)
             qp.setPen(pen)
             qp.drawRect(QRect(x + 5, y, bar_width - 10, height))
 
-            # lbl
+            label = self.labels[i % len(self.labels)]
+            text_x = x + (bar_width // 2) - 10
+            qp.setPen(Qt.white)
+            qp.drawText(text_x, self.height() - 5, label)
 
     def mouseMoveEvent(self, event):
         if not self.data:
@@ -56,8 +64,8 @@ class Week_Histogram(QWidget):
 
         if 0 <= idx < len(self.data):
             max_h = max(self.data)
-            bar_height = int((self.data[idx] / max_h) * (self.height() - 20))
-            bar_top_y = self.height() - bar_height
+            bar_height = int((self.data[idx] / max_h) * (self.height() - 40))
+            bar_top_y = self.height() - bar_height - 20
 
             if event.y() >= bar_top_y:
                 if idx != self.hovered_index:
@@ -67,15 +75,20 @@ class Week_Histogram(QWidget):
                         f"Time spent: {self.data[idx]} hours",
                         self,
                     )
-                    self.setStyleSheet("""QToolTip { 
-                               background-color: #3b434d; 
-                               color: white;
-                               border-radius: 25px;
-                               }""")
-        else:
-            if self.hovered_index is not None:
-                self.hovered_index = None
-                QToolTip.hideText()
-                self.update()
+                    self.setStyleSheet("""
+                        QToolTip { 
+                            background-color: #3b434d; 
+                            color: white;
+                            border-radius: 25px;
+                            padding: 5px;
+                        }
+                    """)
+                    self.update()
+                return
+
+        if self.hovered_index is not None:
+            self.hovered_index = None
+            QToolTip.hideText()
+            self.update()
 
         return super().mouseMoveEvent(event)
